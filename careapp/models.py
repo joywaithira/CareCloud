1766d2bcb55f9efbf9d23b2f4215c42d6430cd54
moses-488 <njorogemoses488@gmail.com>
2026-02-23 10:52:18 +0300

    other commit

--- careapp/models.py
+++ careapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class patient(models.Model):
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=50)
+    dob = models.DateField()
+    age = models.IntegerField()
+    gender = models.CharField(max_length=20)
+    dateregistered = models.DateTimeField()
+    medicalhistory = models.TextField()
+
+
+def __str__(self):
+    return self.firstname + self.lastname
