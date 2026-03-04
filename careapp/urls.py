52440e864aaa0d3e82d52b35cb7f1f8935e56b7a
joywaithira <waithiramaryjoy2@gmail.com>
2026-03-04 10:11:11 +0300

    new

--- careapp/urls.py
+++ careapp/urls.py
@@ -5,7 +5,7 @@ from careapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('home/', views.index, name='index'),
     path('starter/', views.starter, name='starter'),
     path('appointment/', views.appointment, name='appointment'),
     path('about/', views.about, name='about'),
@@ -25,8 +25,8 @@ urlpatterns = [
     path('transactions/', views.transactions_list, name='transactions'),
 
 #Authentication
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
+    path('', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
 
 
 
